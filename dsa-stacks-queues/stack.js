2466b8c4664f9beea7c97aaf69ead5bb2d401e26
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-08-27 15:37:19 -0500

    fully passing tests

--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -20,26 +20,39 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    const newNode = new Node(val)
+    const hold = this.first;
 
+    if (this.first === null) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      hold.next = newNode;
+      this.first = newNode;
+    }
+    this.size++
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.size === 0) throw new Error('ERROR: Stack is empty.')
+    const removeMe = this.first;
+    this.size--
+    return removeMe.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
