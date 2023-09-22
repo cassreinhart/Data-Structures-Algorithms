f2f73b3e43a367ef0cd16b41453785af56abc907
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-09-22 12:19:29 -0500

    update binary trees/initialize bsts

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -16,20 +16,41 @@ class Tree {
 
   sumValues() {
     //below won't work because we have to traverse the whole tree and then sum up all the children...
-    return this.children.reduce((accum, curr) => accum + curr, 0)
+    // return this.children.reduce((accum, curr) => accum + curr, 0)
+
+    let current = this.root;
+    let sum = 0;
+
+    for (let i = 0; i < current.children.length; i++) {
+      for (let val of current.children) {
+        sum += val;
+      }
+      current = current.children[i]
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-    return this.children.reduce((accum, curr) => {
-      if (curr % 2 === 0) {
-        accum + curr
-      } else {
-        accum + 0
-      }
-    }, 0)
+    // return this.children.reduce((accum, curr) => {
+    //   if (curr % 2 === 0) {
+    //     accum + curr
+    //   } else {
+    //     accum + 0
+    //   }
+    // }, 0)
 
+    let current = this.root;
+    let sum = 0;
+
+    for (let i = 0; i < current.children.length; i++) {
+      for (let val of current.children) {
+        if (val % 2 === 0) sum += val;
+      }
+      current = current.children[i]
+    }
+    return sum;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
