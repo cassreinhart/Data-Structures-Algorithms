266a81615fb2ba9b83f2b753d46e108578426203
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-09-27 21:45:38 -0500

    passing all non-further study exercises

--- dsa-sorting/merge.js
+++ dsa-sorting/merge.js
@@ -1,5 +1,42 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let out = [];
+    let a = 0;
+    let b = 0;
 
-function mergeSort() {}
+    while (a < arr1.length && b < arr2.length) {
+        let swapped = false;
+
+        if (arr1[a] < arr2[b]) {
+            out.push(arr1[a])
+            a++
+            swapped = true;
+        } else {
+            out.push(arr2[b])
+            b++
+            swapped = true;
+        }
+        if (!swapped) break;
+    }
+    while (a < arr1.length) {
+        out.push(arr1[a])
+        a++
+    }
+    while (b < arr2.length) {
+        out.push(arr2[b])
+        b++
+    }
+    return out;
+}
+
+function mergeSort(arr) {
+    //base case
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.splice(0, mid))
+    let right = mergeSort(arr.splice(mid))
+    
+    merge(left, right)
+}
 
 module.exports = { merge, mergeSort};
