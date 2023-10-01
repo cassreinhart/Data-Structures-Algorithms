809c2bc9d79d71c9149a0c27161478a432ca12ae
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-09-30 21:36:45 -0500

    passing constructNote & averagePair tests

--- Interview_Prep/prob-freq-count-pointers/average-pair.js
+++ Interview_Prep/prob-freq-count-pointers/average-pair.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, targetAvg) {
+    let left = 0; 
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let avg = (arr[left] + arr[right] / 2);
+        
+        if (avg === targetAvg) return true;
+        else if (avg < targetAvg || avg - targetAvg < 1) {
+            left++
+        } else {
+            right--
+        }
+    }
+    return false
 }
--- Interview_Prep/prob-freq-count-pointers/construct-note.js
+++ Interview_Prep/prob-freq-count-pointers/construct-note.js
@@ -3,8 +3,12 @@ function constructNote(message, letters) {
     function freqCounter(str) {
         let frequencies = {};
 
-        for (let char in str) {
-            frequencies[char] = frequencies[char]++ || 1;
+        for (let i = 0; i < str.length; i++) {
+            let char = str[i];
+            if (!frequencies[char]) frequencies[char] = 1;
+            else {
+                frequencies[char]++
+            }
         }
         return frequencies
     }
@@ -12,11 +16,13 @@ function constructNote(message, letters) {
     let msgFreq = freqCounter(message);
     let lettersFreq = freqCounter(letters);
 
+    console.log(msgFreq, lettersFreq)
+
     for (let char in msgFreq) {
         if (!lettersFreq[char]) return false;
-        if (lettersFreq[char] !== msgFreq[char]) return false;
+        if (lettersFreq[char] < msgFreq[char]) return false;
     }
-    return true
+    return true //there are >= the letters required for the msg
 }
 
 module.exports = constructNote;
